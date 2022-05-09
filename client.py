e547bcd0e8191931da4889d3d228915de4b3652a
MatheusObando <matheus.obando@petitfabrik.com>
2022-05-09 11:39:29 -0400

    Added GUI chat updates

--- client.py
+++ client.py
@@ -1,88 +1,197 @@
+# import all the required modules
 import socket
-import random
-from threading import Thread
-from datetime import datetime
-from common_classes import ChatMessage
-from colorama import Fore, init, Back
-import pickle
+import threading
+from tkinter import *
 
-import nacl.utils
-from nacl.public import PrivateKey, Box
+PORT = 5000
+SERVER = "127.0.0.1"
+ADDRESS = (SERVER, PORT)
+FORMAT = "utf-8"
 
-# init colors
-init()
+# Create a new client socket
+# and connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDRESS)
 
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
 
-# choose a random color for the client
-client_color = random.choice(colors)
+# GUI class for the chat
+class GUI:
+	def __init__(self):
 
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 5002
-SERVER_PORT_KEY = 5003
-separator_token = "<SEP>"
-client_key = PrivateKey.generate()
-client_public_key = client_key.public_key
-foreign_public_key = None
-user_list = ["Teste"]
+		self.Window = Tk()
+		self.Window.withdraw()
+		
+		self.login = Toplevel()
+		self.login.title("Login")
+		self.login.resizable(width = False,
+							height = False)
+		self.login.configure(width = 400,
+							height = 300)
 
-# initialize TCP socket
-s = socket.socket()
-sk = socket.socket()
-print(f"[*] Conectando em {SERVER_HOST}:{SERVER_PORT}...")
-print(f"[*] Conectando em {SERVER_HOST}:{SERVER_PORT_KEY}...")
-# connect to the server
-s.connect((SERVER_HOST, SERVER_PORT))
-sk.connect((SERVER_HOST, SERVER_PORT_KEY))
-print("[+] Conectado.")
-# prompt the client for a name
-name = input("Insira seu nome: ")
+		self.pls = Label(self.login,
+					text = "Please login to continue",
+					justify = CENTER,
+					font = "Helvetica 14 bold")
+		
+		self.pls.place(relheight = 0.15,
+					relx = 0.2,
+					rely = 0.07)
 
-def listen_for_messages():
-    while True:
-        loaded_message = pickle.loads(s.recv(1024))
-        key = loaded_message.key
-        message = loaded_message.message
-        receive_box = Box(client_key, key)
-        decripted_message = receive_box.decrypt(message).decode()
-        decripted_message = decripted_message.replace(separator_token, ": ")
+		self.labelName = Label(self.login,
+							text = "Name: ",
+							font = "Helvetica 12")
+		
+		self.labelName.place(relheight = 0.2,
+							relx = 0.1,
+							rely = 0.2)
 
-        print("\n" + decripted_message)
+		self.entryName = Entry(self.login,
+							font = "Helvetica 14")
+		
+		self.entryName.place(relwidth = 0.4,
+							relheight = 0.12,
+							relx = 0.35,
+							rely = 0.2)
+		
+		self.entryName.focus()
 
-# make a thread that listens for messages to this client & print them
-t = Thread(target=listen_for_messages)
-t.daemon = True
-t.start()
+		self.go = Button(self.login,
+						text = "CONTINUE",
+						font = "Helvetica 14 bold",
+						command = lambda: self.goAhead(self.entryName.get()))
+		
+		self.go.place(relx = 0.4,
+					rely = 0.55)
+		self.Window.mainloop()
 
-while True:
-    sk.sendall(pickle.dumps(client_public_key))
-    foreign_public_key = pickle.loads(sk.recv(1024))
-    # print(foreign_public_key)
-    # print(client_key)
-    while True:
-        # input message we want to send to the server
-        to_send =  input()
-        if to_send.lower() == 'q':
-            break
-        if to_send == "/?":
-            print(user_list)
+	def goAhead(self, name):
+		self.login.destroy()
+		self.layout(name)
+		rcv = threading.Thread(target=self.receive)
+		rcv.start()
+        
+	def layout(self,name):
+	
+		self.name = name
+		# to show chat window
+		self.Window.deiconify()
+		self.Window.title("CHATROOM")
+		self.Window.resizable(width = False,
+							height = False)
+		self.Window.configure(width = 470,
+							height = 550,
+							bg = "#17202A")
+		self.labelHead = Label(self.Window,
+							bg = "#17202A",
+							fg = "#EAECEE",
+							text = self.name ,
+							font = "Helvetica 13 bold",
+							pady = 5)
+		
+		self.labelHead.place(relwidth = 1)
+		self.line = Label(self.Window,
+						width = 450,
+						bg = "#ABB2B9")
+		
+		self.line.place(relwidth = 1,
+						rely = 0.07,
+						relheight = 0.012)
+		
+		self.textCons = Text(self.Window,
+							width = 20,
+							height = 2,
+							bg = "#17202A",
+							fg = "#EAECEE",
+							font = "Helvetica 14",
+							padx = 5,
+							pady = 5)
+		
+		self.textCons.place(relheight = 0.745,
+							relwidth = 1,
+							rely = 0.08)
+		
+		self.labelBottom = Label(self.Window,
+								bg = "#ABB2B9",
+								height = 80)
+		
+		self.labelBottom.place(relwidth = 1,
+							rely = 0.825)
+		
+		self.entryMsg = Entry(self.labelBottom,
+							bg = "#2C3E50",
+							fg = "#EAECEE",
+							font = "Helvetica 13")
+		
+		self.entryMsg.place(relwidth = 0.74,
+							relheight = 0.06,
+							rely = 0.008,
+							relx = 0.011)
+		
+		self.entryMsg.focus()
+		
+		# create a Send Button
+		self.buttonMsg = Button(self.labelBottom,
+								text = "Send",
+								font = "Helvetica 10 bold",
+								width = 20,
+								bg = "#ABB2B9",
+								command = lambda : self.sendButton(self.entryMsg.get()))
+		
+		self.buttonMsg.place(relx = 0.77,
+							rely = 0.008,
+							relheight = 0.06,
+							relwidth = 0.22)
+		
+		self.textCons.config(cursor = "arrow")
+		
+		# create a scroll bar
+		scrollbar = Scrollbar(self.textCons)
+		
+		# place the scroll bar
+		# into the gui window
+		scrollbar.place(relheight = 1,
+						relx = 0.974)
+		
+		scrollbar.config(command = self.textCons.yview)
+		
+		self.textCons.config(state = DISABLED)
 
-        date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        message = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
-        res = bytes(message, 'utf-8')
+	# function to basically start the thread for sending messages
+	def sendButton(self, msg):
+		self.textCons.config(state = DISABLED)
+		self.msg=msg
+		self.entryMsg.delete(0, END)
+		snd= threading.Thread(target = self.sendMessage)
+		snd.start()
 
-        send_box = Box(client_key, foreign_public_key)
-        encrypted_msg = send_box.encrypt(res)
-        chat_msg = ChatMessage(encrypted_msg, client_public_key)
-        s.send(pickle.dumps(chat_msg))
+	# function to receive messages
+	def receive(self):
+		while True:
+			try:
+				message = client.recv(1024).decode(FORMAT)
+				
+				# if the messages from the server is NAME send the client's name
+				if message == 'NAME':
+					client.send(self.name.encode(FORMAT))
+				else:
+					# insert messages to text box
+					self.textCons.config(state = NORMAL)
+					self.textCons.insert(END, message+"\n\n")
+					
+					self.textCons.config(state = DISABLED)
+					self.textCons.see(END)
+			except:
+				# an error will be printed on the command line or console if there's an error
+				print("An error occured!")
+				client.close()
+				break
+		
+	# function to send messages
+	def sendMessage(self):
+		self.textCons.config(state=DISABLED)
+		while True:
+			message = (f"{self.name}: {self.msg}")
+			client.send(message.encode(FORMAT))
+			break
 
-# close the socket
-s.close()
-sk.close()
+guiInstance = GUI()
--- server.py
+++ server.py
@@ -1,88 +1,84 @@
 import socket
-import pickle
-import sys
-import traceback
-from common_classes import ChatMessage
-import copy
-from threading import Thread
-
-import nacl.utils
-from nacl.public import PrivateKey, Box
-
-# server's IP address
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5002
-SERVER_PORT_KEY = 5003
-server_key = PrivateKey.generate()
-server_public_key = server_key.public_key
-
-# initialize list/set of all connected client's sockets
-client_sockets = set()
-# create a TCP socket
-s = socket.socket()
-sk = socket.socket()
-
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# bind the socket to the address we specified
-s.bind((SERVER_HOST, SERVER_PORT))
-sk.bind((SERVER_HOST, SERVER_PORT_KEY))
-
-sk.listen(2)
-s.listen(2)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT_KEY}")
-
-def listen_for_client(cs):
-
+import threading
+ 
+PORT = 5000
+SERVER = "0.0.0.0"
+ 
+# Address is stored as a tuple
+ADDRESS = (SERVER, PORT)
+FORMAT = "utf-8"
+ 
+# Lists that will contains
+# all the clients connected to
+# the server and their names.
+clients, names = [], []
+ 
+# Create a new socket for
+# the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# bind the address of the
+# server to the socket
+server.bind(ADDRESS)
+ 
+# function to start the connection
+def startChat():
+   
+    print("server is working on " + SERVER)
+     
+    # listening for connections
+    server.listen()
+     
     while True:
-        try:
-            loaded_message = pickle.loads(cs.recv(1024))
-            key = loaded_message.key
-            message = loaded_message.message
-            receive_box = Box(server_key, key)
-            decripted_message = receive_box.decrypt(message)
-            loaded_message.key = server_public_key
-            loaded_message.message = decripted_message
-
-        except Exception as e:
-            print(f"[!] Error: {e}")
-            client_sockets.remove(cs)
-            print(sys.exc_info())
-            exit(-1)
-
-        else:
-            for client_socket in client_sockets:
-                new_message = copy.deepcopy(loaded_message)
-                key = new_message.key
-                message = new_message.message
-                send_box = Box(server_key, client_socket[1])
-                new_message.message = send_box.encrypt(message)
-                serialized_data = pickle.dumps(new_message)
-                client_socket[0].sendall(serialized_data)
-
-            del(loaded_message)
-
-while True:
-    try: 
-        client_socket_key, client_address_key = sk.accept()
-        client_socket, client_address = s.accept()
-        client_public_key = pickle.loads(client_socket_key.recv(1024))
-        client_socket_key.send(pickle.dumps(server_public_key))
-        print(f"[+] {client_address} conectou.\nChave: {client_public_key}\n")
-        client_sockets.add((client_socket, client_public_key))
-    except Exception as e:
-        print(f"[!] Error: {e}")
-        print(sys.exc_info())
-        exit(-1)
-    
-
-    t = Thread(target=listen_for_client, args=(client_socket,))
-    t.daemon = True
-    t.start()
-
-# close client sockets
-for cs in client_sockets:
-    cs[0].close()
-# close server socket
-s.close()
+       
+        # accept connections and returns
+        # a new connection to the client
+        #  and  the address bound to it
+        conn, addr =  server.accept()
+        conn.send("NAME".encode(FORMAT))
+         
+        # 1024 represents the max amount
+        # of data that can be received (bytes)
+        name = conn.recv(1024).decode(FORMAT)
+         
+        # append the name and client
+        # to the respective list
+        names.append(name)
+        clients.append(conn)
+         
+        print(f"Name is :{name}")
+         
+        # broadcast message
+        broadcastMessage(f"{name} has joined the chat!".encode(FORMAT))
+        conn.send('Connection successful!'.encode(FORMAT))
+         
+        # Start the handling thread
+        thread = threading.Thread(target = handle, args = (conn, addr))
+        thread.start()
+         
+        # no. of clients connected
+        # to the server
+        print(f"active connections {threading.active_count()-1}")
+ 
+# method to handle the
+# incoming messages
+def handle(conn, addr):
+   
+    print(f"new connection {addr}")
+    connected = True
+     
+    while connected:
+          # receive message
+        message = conn.recv(1024)
+         
+        # broadcast message
+        broadcastMessage(message)
+     
+    # close the connection
+    conn.close()
+ 
+def broadcastMessage(message):
+    for client in clients:
+        client.send(message)
+ 
+startChat()
